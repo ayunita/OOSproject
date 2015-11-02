041481784528707f576d6ae3e27437e436e7c7a8
ayunita <yunita@ualberta.ca>
2015-11-02 14:14:39 -0700

    admin page is done

--- oos/PHPconnectionDB.php
+++ oos/PHPconnectionDB.php
@@ -1,6 +1,6 @@
 <?php
 function connect(){
-	$conn = oci_connect('', '');
+	$conn = oci_connect('username', 'password');
 	if (!$conn) {
 		$e = oci_error();
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
--- oos/administrator.php
+++ oos/administrator.php
@@ -81,8 +81,95 @@
 		</form>
 	</div>
 	<div id="edit_user_btn">Manage User</div>
-	<div id="edit_user_panel">
-		Do search by person id then edit user or person, person id cannot be changed!
+	<?php 
+		if (isset($_POST["search_person"]) || isset($_POST["search_user"])){
+			echo '<div id="edit_user_panel" style="display:inherit">';
+		} else {
+			echo '<div id="edit_user_panel">';
+		}
+	?>
+		<form action="" method="post">
+			<b>Edit person information</b><br />
+			Search person_id: <input type="text" name="edit_personid">
+			<input type="submit" name="search_person" value="Search">
+		</form>
+	<?php 
+		if (isset($_POST["search_person"])){
+			$edit_personid=$_POST['edit_personid'];
+			$sql = "SELECT * FROM persons WHERE person_id=".$edit_personid;
+			$stid = oci_parse($conn, $sql );
+			$res=oci_execute($stid);
+
+			$isFound = false;
+			while(oci_fetch($stid)){
+				$person_firstname =oci_result($stid, 'FIRST_NAME');
+				$person_lastname =oci_result($stid, 'LAST_NAME');
+				$person_address =oci_result($stid, 'ADDRESS');
+				$person_email =oci_result($stid, 'EMAIL');
+				$person_phone =oci_result($stid, 'PHONE');
+				$isFound = true;
+			}
+			
+			if($isFound == false){
+				echo 'Person id: '.$edit_personid.' does not exist.';
+			} else {
+				echo '<br /><form action="" method="post">';
+				echo 'Person id: <input type="text" name="dis_personid" value="'.$edit_personid.'" readonly> <br /><br />';
+				echo 'Firstname: <input type="text" name="edit_firstname" value="'.$person_firstname.'"> <br /><br />';
+				echo 'Lastname: <input type="text" name="edit_lastname" value="'.$person_lastname.'"> <br /><br />';
+				echo 'Address: <input type="text" name="edit_address" value="'.$person_address.'"> <br /><br />';
+				echo 'Email: <input type="text" name="edit_email" value="'.$person_email.'"> <br /><br />';
+				echo 'Phone: <input type="text" name="edit_phone" value="'.$person_phone.'"> <br /><br />';
+				echo '<input type="submit" name="edit_person" value="Edit">';
+				echo '</form>';
+			}
+			
+			// Free the statement identifier when closing the connection
+			oci_free_statement($stid);
+			oci_close($conn);
+		}
+	?>
+
+		<br /><form action="" method="post">
+			<b>Edit user information</b><br />
+			Search username: <input type="text" name="search_username">
+			<input type="submit" name="search_user" value="Search">
+		</form>
+
+	<?php 
+		if (isset($_POST["search_user"])){
+			$search_username=$_POST['search_username'];
+			$sql = "SELECT * FROM users WHERE user_name='".$search_username."'";
+			$stid = oci_parse($conn, $sql );
+			$res=oci_execute($stid);
+
+			$isFound = false;
+			while(oci_fetch($stid)){
+				$user_username =oci_result($stid, 'USER_NAME');
+				$user_password =oci_result($stid, 'PASSWORD');
+				$user_role =oci_result($stid, 'ROLE');
+				$user_id =oci_result($stid, 'PERSON_ID');
+				$isFound = true;
+			}
+			
+			if($isFound == false){
+				echo 'Username: '.$search_username.' does not exist.';
+			} else {
+				echo '<br /><form action="" method="post">';
+				echo 'Username: <input type="text" name="edit_username" value="'.$user_username.'" readonly> <br /><br />';
+				echo 'Password: <input type="text" name="edit_password" value="'.$user_password.'"> <br /><br />';
+				echo 'Role: <input type="text" name="edit_role" value="'.$user_role.'"> <br /><br />';
+				echo 'Person id: <input type="text" name="edit_personid" value="'.$user_id.'"> <br /><br />';
+				echo '<input type="submit" name="edit_user" value="Edit">';
+				echo '</form>';
+			}
+			
+			// Free the statement identifier when closing the connection
+			oci_free_statement($stid);
+			oci_close($conn);
+		}
+	?>
+
 	</div>
 	<div id="del_user_btn">Delete User</div>
 	<div id="del_user_panel">
@@ -242,8 +329,45 @@
 			oci_close($conn);
 		}
 		
-		
-		
+		if (isset($_POST["edit_person"])){
+			$dis_personid=$_POST['dis_personid'];
+			$edit_firstname=$_POST['edit_firstname'];
+			$edit_lastname=$_POST['edit_lastname'];
+			$edit_address=$_POST['edit_address'];
+			$edit_email=$_POST['edit_email'];
+			$edit_phone=$_POST['edit_phone'];
+
+			$sql = "UPDATE persons SET first_name='".$edit_firstname."', last_name='".$edit_lastname.
+				"', address='".$edit_address."', email='".$edit_email.
+				"', phone='".$edit_phone."' WHERE person_id=".$dis_personid;
+			$stid = oci_parse($conn, $sql );
+			$res=oci_execute($stid);
+			
+			echo "UPDATED: ".$dis_personid.", ".$edit_firstname." ".$edit_lastname.", ".$edit_address.", ".$edit_email.", ".$edit_phone;
+
+			// Free the statement identifier when closing the connection
+			oci_free_statement($stid);
+			oci_close($conn);
+		}
+
+		if (isset($_POST["edit_user"])){
+			$user_username=$_POST['edit_username'];
+			$user_password=$_POST['edit_password'];
+			$user_role=$_POST['edit_role'];
+			$user_id=$_POST['edit_personid'];
+			
+			$sql = "UPDATE users SET password='".$user_password."', role='".$user_role.
+						"', person_id=".$user_id." WHERE user_name='".$user_username."'";
+			
+			$stid = oci_parse($conn, $sql );
+			$res=oci_execute($stid);
+			
+			echo "UPDATED: ".$user_username.", ".$user_password." ".$user_role.", ".$user_id;
+
+			// Free the statement identifier when closing the connection
+			oci_free_statement($stid);
+			oci_close($conn);
+		}
 	?>
 	</span>
 		
