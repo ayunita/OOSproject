a827361181728dd56667617a25e92773659a0ddf
CH16 <chen1@cs.ualberta.ca>
2015-11-02 16:37:32 -0700

    All sensors and all subscribed sensors done

--- oos/subscribe.php
+++ oos/subscribe.php
@@ -43,7 +43,7 @@
 				echo "<th>Sensor Type</th><th>Description</th></tr>";
 
 				while (oci_fetch($stid)) {
-    					echo "<tr><td>".oci_result($stid, 'SENSOR_ID') . "</td>";
+    				echo "<tr><td>".oci_result($stid, 'SENSOR_ID') . "</td>";
   					echo "<td>".oci_result($stid, 'LOCATION') . "</td>";
 					echo "<td>".oci_result($stid, 'SENSOR_TYPE')."</td>";
 					echo "<td>".oci_result($stid, 'DESCRIPTION')."</td></tr>";
@@ -51,8 +51,6 @@
 				echo "</table>";
 
 				oci_free_statement($stid);
-				oci_close($conn);
-
 
 				?>
 			</fieldset>
@@ -64,6 +62,11 @@
 		<form action="" method="post">
 			<fieldset>
 				<?php
+
+				$sql = "SELECT * FROM subscriptions WHERE PERSON_ID = ".$_SESSION['person_id'];
+				$stid = oci_parse($conn, $sql );
+				oci_execute($stid);
+				
 				echo "<style>
 					table, th, td {
 					    border: 1px solid black;
@@ -71,9 +74,29 @@
 					</style>";
 				echo "<table><tr><th>Sensor ID</th><th>Location</th>";
 				echo "<th>Sensor Type</th><th>Description</th></tr>";
+								
+				$sensor_ids = array();
+				$i = 0;
+				while (oci_fetch($stid)) {
+  					$sensor_ids[$i] = oci_result($stid, 'SENSOR_ID');
+  					$i = $i + 1;
+				}
+				oci_free_statement($stid);
+				
+				for ($i2 = 0; $i2 < $i; $i2++){
+					$sql = "select * from sensors where SENSOR_ID = ".$sensor_ids[$i2];
+					$stid = oci_parse($conn, $sql);
+					oci_execute($stid);
+					while (oci_fetch($stid)) {
+    					echo "<tr><td>".oci_result($stid, 'SENSOR_ID') . "</td>";
+  						echo "<td>".oci_result($stid, 'LOCATION') . "</td>";
+						echo "<td>".oci_result($stid, 'SENSOR_TYPE')."</td>";
+						echo "<td>".oci_result($stid, 'DESCRIPTION')."</td></tr>";
+				}
+					oci_free_statement($stid);
+				}
 
 				echo "</table>";
-
 
 
 				?>
