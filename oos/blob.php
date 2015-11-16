610945af2b9ba65e6eb17f692afc805ca83c3d7b
ayunita <yunita@ualberta.ca>
2015-11-16 16:08:48 -0700

    working on load audio and image

--- oos/blob.php
+++ oos/blob.php
@@ -12,19 +12,19 @@
 	$connection=connect();
 
     if (isset($_POST["upload"])){
-        $audio2 = file_get_contents($_FILES['file']['tmp_name']);
+        $image2 = file_get_contents($_FILES['file']['tmp_name']);
         // encode the stream
-        $audio = base64_encode($audio2);
+        $image = base64_encode($image2);
         
         // change this sql query, this is just an example
 		$id = rand(1000, 9999);
-        $sql = "INSERT INTO test (id, audio) VALUES(".$id.", empty_blob()) RETURNING audio INTO :audio";
+        $sql = "INSERT INTO test2 (id, image) VALUES(".$id.", empty_blob()) RETURNING image INTO :image";
         $result = oci_parse($connection, $sql);
         $blob = oci_new_descriptor($connection, OCI_D_LOB);
-        oci_bind_by_name($result, ":audio", $blob, -1, OCI_B_BLOB);
+        oci_bind_by_name($result, ":image", $blob, -1, OCI_B_BLOB);
         oci_execute($result, OCI_DEFAULT) or die ("Unable to execute query");
         
-        if(!$blob->save($audio)) {
+        if(!$blob->save($image)) {
             oci_rollback($connection);
         }
         else {
@@ -35,3 +35,8 @@
         $blob->free();
     }
 ?>
+
+<!--
+for uploading audio:
+change "image" to "audio"
+-->
--- oos/load.php
+++ oos/load.php
@@ -1,3 +1,8 @@
+<!--
+	This example used:
+	TABLE test, COLUMN id (number), audio (blob)
+	TABLE test2, COLUMN id (number), image (blob)
+-->
 <?php
     
     /*
@@ -11,25 +16,108 @@
 	//establish connection
 	$conn=connect();
 
-    $sql = "SELECT audio FROM test WHERE id = 8869";
-    $stid = oci_parse($conn, $sql);
-    oci_execute($stid);
-    $row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS);
-    if (!$row) {
-        header('Status: 404 Not Found');
-    } else {
-        $wav = $row['AUDIO']->load();
-        // header("Content-type: audio/wav");
-        
-        // decode the stream
-	$wav_source = base64_decode($wav);
-	
-    }
 ?>
+
 <html>
-<head><title>Load</title></head>
-<body>
-<!-- .wav player works in Chrome -->
-<audio controls src="data:audio/wav;base64,<?php print $wav; ?>"/>
-</body>
+	<head><title>Load</title></head>
+	<body>
+		<form action="" method="post">
+		Search audio: <select name="audios" value="">audio id</option>
+		<?php
+			$sql = "SELECT id FROM test";
+			$stid = oci_parse($conn, $sql );
+			$res = oci_execute($stid);
+					
+			while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+				foreach ($row as $id) {
+					echo "<option value=$id>$id</option>"; 
+				}
+			}
+		?>
+			<input type="submit" name="search_audio" value="Search">
+		</form>
+		
+		<form action="" method="post">
+		Search image: <select name="images" value="">image id</option>
+		<?php
+			$sql = "SELECT id FROM test2";
+			$stid = oci_parse($conn, $sql );
+			$res = oci_execute($stid);
+					
+			while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+				foreach ($row as $id) {
+					echo "<option value=$id>$id</option>"; 
+				}
+			}
+		?>
+			<input type="submit" name="search_image" value="Search">
+		</form>
+
+<!-- Search audio example -->
+<?php
+	if (isset($_POST["search_audio"])){
+		$audio_id=$_POST['audios'];
+		$sql = "SELECT audio FROM test WHERE id =".$audio_id;
+		$stid = oci_parse($conn, $sql);
+		oci_execute($stid);
+		$row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS);
+		if (!$row) {
+			header('Status: 404 Not Found');
+		} else {
+			$wav = $row['AUDIO']->load();
+			$decoded = base64_decode($wav);
+			
+			/*
+			* This is the trick for convert string base64 to file and download it:
+			* - have one dummy file.type
+			* - chmod 777 dummy
+			* - then rewrite (file_put_contents) the decoded string to this dummy file.type
+			* - downloading file points to dummy file.type url
+			*/
+			file_put_contents('audio.wav', $decoded);
+			
+			// play the audio
+			echo '<audio controls>';
+			echo '<source src="audio.wav" type="audio/wav">';
+			echo '</audio>';
+			
+			// download the audio
+			echo '<br /><a href="audio.wav" download="'.$audio_id.'.wav">Download audio</a>';
+		}
+	}
+?>
+
+<!-- Search image example -->	
+<?php
+	if (isset($_POST["search_image"])){
+		$image_id=$_POST['images'];
+		$sql = "SELECT image FROM test2 WHERE id =".$image_id;
+		$stid = oci_parse($conn, $sql);
+		oci_execute($stid);
+		$row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS);
+		if (!$row) {
+			header('Status: 404 Not Found');
+		} else {
+			$img = $row['IMAGE']->load();
+			$decoded = base64_decode($img);
+			
+			// display image (no need decoded)
+			echo '<img src="data:image/gif;base64,'.$img.'" />';
+			
+			/*
+			* This is the trick for convert string base64 to file and download it:
+			* - have one dummy file.type
+			* - chmod 777 dummy
+			* - then rewrite (file_put_contents) the decoded string to this dummy file.type
+			* - downloading file points to dummy file.type url
+			*/
+			file_put_contents('sensor.jpg', $decoded);
+			
+			// download the audio
+			echo '<br /><a href="sensor.jpg" download="'.$image_id.'.jpg">Download image</a>';
+		}
+	}
+?>
+		
+	</body>
 </html>
