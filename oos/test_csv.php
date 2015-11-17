65081a7fa1c8b70bea8aca6e2138006457c58940
CH16 <chen1@cs.ualberta.ca>
2015-11-17 16:18:54 -0700

    search final draft, can print out scalar, but no image and audio in database for testing

---
+++ oos/test_csv.php
@@ -0,0 +1,18 @@
+<?php
+
+$list = array (
+    array('aaa', 'bbb', 'ccc', 'dddd'),
+    array('123', '456', '789'),
+    array('"aaa"', '"bbb"')
+);
+
+$fp = fopen('file.csv', 'w');
+
+foreach ($list as $fields) {
+    fputcsv($fp, $fields);
+}
+
+fclose($fp);
+?>
+
+<a href="file.csv" download="sensor.csv">Download file</a>
