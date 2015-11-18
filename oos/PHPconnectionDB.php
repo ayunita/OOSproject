90712bfad90361e02d33bceff805f0e1f9d2a610
ayunita <yunita@ualberta.ca>
2015-11-17 18:18:41 -0700

    modified readme

--- oos/PHPconnectionDB.php
+++ oos/PHPconnectionDB.php
@@ -1,6 +1,6 @@
 <?php
 function connect(){
-	$conn = oci_connect('yunita', 'yenchi12');
+	$conn = oci_connect('', '');
 	if (!$conn) {
 		$e = oci_error();
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
