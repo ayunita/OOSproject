ad98abce8c290febeecb4a48656e8ad8037f714a
CH16 <chen1@cs.ualberta.ca>
2015-11-20 16:58:09 -0700

    trying to format csv

--- oos/Search.php
+++ oos/Search.php
@@ -46,103 +46,97 @@
 				for ($i2 = 0; $i2 < $i; $i2++){
 					echo "<br />Sensors that satisfy the conditions:<br>Sensor ID: ".$sensor_ids[$i2]."<br />";
 					$match  = true;
-					$search_string_image = "select thumbnail, recoreded_data from images where SENSOR_ID = ".$sensor_ids[$i2];
-					$search_string_audio = "select recorded_data from audio_recordings where SENSOR_ID = ".$sensor_ids[$i2];
-					$search_string_scalar = "select SENSOR_ID, TO_CHAR(DATE_CREATED, 'DD/MM/YYYY HH24:MI:SS') as DATE_CREATED, VALUE from scalar_data where SENSOR_ID = ".$sensor_ids[$i2];
+					$search_string_image = "select thumbnail, recoreded_data from images where SENSOR_ID = ".$sensor_ids[$i2]." AND date_created between TO_DATE('".$_POST['from']."','DD/MM/YYYY hh24:mi:ss') and TO_DATE('".$_POST['to']."','DD/MM/YYYY hh24:mi:ss')";
+					$search_string_audio = "select recorded_data from audio_recordings where SENSOR_ID = ".$sensor_ids[$i2]." AND date_created between TO_DATE('".$_POST['from']."','DD/MM/YYYY hh24:mi:ss') and TO_DATE('".$_POST['to']."','DD/MM/YYYY hh24:mi:ss')";
+					$search_string_scalar = "select SENSOR_ID, TO_CHAR(DATE_CREATED, 'DD/MM/YYYY HH24:MI:SS') as DATE_CREATED, VALUE from scalar_data where SENSOR_ID = ".$sensor_ids[$i2]." AND date_created between TO_DATE('".$_POST['from']."','DD/MM/YYYY hh24:mi:ss') and TO_DATE('".$_POST['to']."','DD/MM/YYYY hh24:mi:ss')";
 			
-					if(strval($_POST['from']) != "") {
-						$search_string_image = $search_string_image." AND date_created >=  TO_DATE('".$_POST['from']."','DD/MM/YYYY HH24:MI:SS')";
-						$search_string_audio = $search_string_audio." AND date_created >=  TO_DATE('".$_POST['from']."','DD/MM/YYYY HH24:MI:SS')";
-						$search_string_scalar = $search_string_scalar." AND date_created >=  TO_DATE('".$_POST['from']."','DD/MM/YYYY HH24:MI:SS')";
-					}
-					if(strval($_POST['to']) != "") {
-						$search_string_image = $search_string_image." AND date_created <=  TO_DATE('".$_POST['to']."','DD/MM/YYYY HH24:MI:SS')";
-						$search_string_audio = $search_string_audio." AND date_created <=  TO_DATE('".$_POST['to']."','DD/MM/YYYY HH24:MI:SS')";
-						$search_string_scalar = $search_string_scalar." AND date_created <=  TO_DATE('".$_POST['to']."','DD/MM/YYYY HH24:MI:SS')";
-					}
-						//echo "<br>".$search_string_image."<br>";
-						//echo "<br>".$search_string_audio."<br>";
-						//echo "<br>".$search_string_scalar."<br>";
 
-						$stid = oci_parse($conn, $search_string_scalar);
-						oci_execute($stid);
+					//echo "<br>".$search_string_image."<br>";
+					//echo "<br>".$search_string_audio."<br>";
+					//echo "<br>".$search_string_scalar."<br>";
+			
+					$stid = oci_parse($conn, $search_string_scalar);
+					oci_execute($stid);
 						
-						//scalar
-						$i3 = 0;
-						while (oci_fetch($stid)) {
-							echo "<br>".oci_result($stid, 'SENSOR_ID'),",",oci_result($stid, 'DATE_CREATED'),",",oci_result($stid, 'VALUE');
-							$list = array(oci_result($stid, 'SENSOR_ID'),oci_result($stid, 'DATE_CREATED'),oci_result($stid, 'VALUE'));
-							array_push($lists,$list);
-							$i3 = $i3+1;
+					//scalar
+					$i3 = 0;
+					while (oci_fetch($stid)) {
+						$list = array(oci_result($stid, 'SENSOR_ID'),date(oci_result($stid, 'DATE_CREATED')),oci_result($stid, 'VALUE'));
+						$scalar_data = oci_result($stid, 'SENSOR_ID').",".oci_result($stid, 'DATE_CREATED').",".oci_result($stid, 'VALUE');
+						echo "<br>".$scalar_data;
+						array_push($lists,$list);
+						$i3 = $i3+1;
 							
-						}
-						oci_free_statement($stid);
+					}
+					//echo $i3."<br/><br/><br/><br/>";
+					oci_free_statement($stid);
 
-						//fetch image
-						$stid = oci_parse($conn,$search_string_image);
-						oci_execute($stid);
+					//fetch image
+					$stid = oci_parse($conn,$search_string_image);
+					oci_execute($stid);
 												
-						while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS )){	
+					while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS )){	
 						
-							if ($row['THUMBNAIL'] != null && $row['RECOREDED_DATA'] != null){
-								$img = $row['THUMBNAIL']->load();
+						if ($row['THUMBNAIL'] != null && $row['RECOREDED_DATA'] != null){
+							$img = $row['THUMBNAIL']->load();
 			
-								// display thumbnail (no need decoded)
-								echo '<br><img src="data:image/jpg;base64,'.$img.'" />';
+							// display thumbnail (no need decoded)
+							echo '<br><img src="data:image/jpg;base64,'.$img.'" />';
 			
-								$img2 = $row['RECOREDED_DATA']->load();
+							$img2 = $row['RECOREDED_DATA']->load();
 
-								// download the full size image
-								echo '<br /><a href="data:image/jpg;base64,'.$img2.'" download="'.$sensor_ids[$i2].'.jpg">Download image</a>';
-								//oci_free_statement($stid);
+							// download the full size image
+							echo '<br /><a href="data:image/jpg;base64,'.$img2.'" download="'.$sensor_ids[$i2].'.jpg">Download image</a>';
+							//oci_free_statement($stid);
 							
-							} else if($row['RECOREDED_DATA'] == null){
-								//echo "<br />THIS SENSOR HAS NO IMAGE<br />";
-							}
+						} else if($row['RECOREDED_DATA'] == null){
+							//echo "<br />THIS SENSOR HAS NO IMAGE<br />";
 						}
-						$num = oci_num_rows($stid);							
-						
-						//fetch audio
-						$stid = oci_parse($conn, $search_string_audio);
-						oci_execute($stid);
-							
-						while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)){
-							if (!$row) {
-								//echo '<br>Status: 404 Not Found<br>';
-							}
-							if ($row['RECORDED_DATA'] != null){
-								$wav = $row['RECORDED_DATA']->load();
-								$decoded = base64_decode($wav);
-								
-								// play the audio
-								echo '<audio controls>';
-								// change src to "data:audio/wav;base64,'.$wav.'"
-								echo '<source src="data:audio/wav;base64,'.$wav.'">';
-								echo '</audio>';
-				
-								// download the audio
-								echo '<br /><a href="data:audio/wav;base64,'.$wav.'" download="'.$sensor_ids[$i2].'.wav">Download audio</a></br>';
-								//Audio
-						
-							}else if ($row['RECORDED_DATA'] == null){
-								//echo "<br />THIS SENSOR HAS NO AUDIO<br />";
-							}	
-						}
-						$num = oci_num_rows($stid);		
-						echo "<hr>";
-					
 					}
-
-					if ($lists != null){
-						$fp = fopen('file.csv', 'w');
-						foreach ($lists as $fields) {
-	   					fputcsv($fp, $fields, ", ");
+					$num = oci_num_rows($stid);							
+						
+					//fetch audio
+					$stid = oci_parse($conn, $search_string_audio);
+					oci_execute($stid);
+							
+					while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)){
+						if (!$row) {
+							//echo '<br>Status: 404 Not Found<br>';
 						}
-						fclose($fp);
-
-						echo "<br><a href=\"file.csv\" download=\"scalar_data.csv\">Download Scalar</a>";	
-					}				
+						if ($row['RECORDED_DATA'] != null){
+							$wav = $row['RECORDED_DATA']->load();
+							$decoded = base64_decode($wav);
+								
+							// play the audio
+							echo '<audio controls>';
+							// change src to "data:audio/wav;base64,'.$wav.'"
+							echo '<source src="data:audio/wav;base64,'.$wav.'">';
+							echo '</audio>';
+				
+							// download the audio
+							echo '<br /><a href="data:audio/wav;base64,'.$wav.'" download="'.$row['recording_id'].'.wav">Download audio</a></br>';
+							//Audio
+						
+						}else if ($row['RECORDED_DATA'] == null){
+							//echo "<br />THIS SENSOR HAS NO AUDIO<br />";
+						}	
+					}
+					$num = oci_num_rows($stid);		
+					echo "<hr>";
+					
 				}
+
+				if ($lists != null){
+					$fp = fopen('file.csv', 'w');
+
+					foreach ($lists as $fields) {
+	   				fputcsv($fp, $fields, ", ");
+					}
+					fclose($fp);
+
+					echo "<br><a href=\"file.csv\" download=\"scalar_data.csv\">Download Scalar</a>";	
+				}				
+			}
 			
 
 			if ($i == 0 ){
@@ -172,17 +166,10 @@
 	
 				for ($i2 = 0; $i2 < $i; $i2++){
 					
-					$search_string_image = "select thumbnail, recoreded_data from images where SENSOR_ID = ".$sensor_ids[$i2];
-					$search_string_audio = "select recorded_data from audio_recordings where SENSOR_ID = ".$sensor_ids[$i2];
+					$search_string_image = "select thumbnail, recoreded_data from images where SENSOR_ID = ".$sensor_ids[$i2]." AND date_created between TO_DATE('".$_POST['from']."','DD/MM/YYYY hh24:mi:ss') and TO_DATE('".$_POST['to']."','DD/MM/YYYY hh24:mi:ss')";;
+					$search_string_audio = "select recorded_data from audio_recordings where SENSOR_ID = ".$sensor_ids[$i2]." AND date_created between TO_DATE('".$_POST['from']."','DD/MM/YYYY hh24:mi:ss') and TO_DATE('".$_POST['to']."','DD/MM/YYYY hh24:mi:ss')";;
 			
-					if(strval($_POST['from']) != "") {
-						$search_string_image = $search_string_image." AND date_created >=  TO_DATE('".$_POST['from']."','DD-MM-YYYY')";
-						$search_string_audio = $search_string_audio." AND date_created >=  TO_DATE('".$_POST['from']."','DD-MM-YYYY')";
-					}
-					if(strval($_POST['to']) != "") {
-						$search_string_image = $search_string_image." AND date_created <=  TO_DATE('".$_POST['to']."','DD-MM-YYYY')";
-						$search_string_audio = $search_string_audio." AND date_created <=  TO_DATE('".$_POST['to']."','DD-MM-YYYY')";
-					}
+
 					if(strval($_POST['description']) != "") {
 						$search_string_image = $search_string_image." AND description like '%".$_POST['description']."%'";
 						$search_string_audio = $search_string_audio." AND description like '%".$_POST['description']."%'";
