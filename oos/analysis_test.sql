218b34f498acabd2a1d990a29e84bd07c4e478ab
ayunita <yunita@ualberta.ca>
2015-11-22 18:25:50 -0700

    new fact table sql

--- oos/analysis_test.sql
+++ oos/analysis_test.sql
@@ -7,19 +7,22 @@ CREATE TABLE fact_table
 AS SELECT *
 FROM 
 (
-  SELECT *
-  FROM
-  (
-    SELECT s.sensor_id, s.location, d.date_created, 
-    AVG(d.value) as AVG, MAX(d.value) as MAX, MIN(d.value) as MIN
-    FROM sensors s, scalar_data d
-    WHERE s.sensor_id = d.sensor_id
-    AND s.sensor_type = 's'
-    GROUP BY CUBE(s.sensor_id, s.location, d.date_created)
-  )
-  WHERE sensor_id is not null
-  AND location is not null
-  AND date_created is not null
+    SELECT sensor_id, location, TO_CHAR(date_created, 'YYYY') as year, 
+      TO_CHAR(date_created, 'Mon') as month, 
+      TO_CHAR(date_created, 'DD') as day,
+      AVG, MIN, MAX
+    FROM
+    (
+      SELECT s.sensor_id, s.location, d.date_created, 
+      AVG(d.value) as AVG, MAX(d.value) as MAX, MIN(d.value) as MIN
+      FROM sensors s, scalar_data d
+      WHERE s.sensor_id = d.sensor_id
+      AND s.sensor_type = 's'
+      GROUP BY CUBE(s.sensor_id, s.location, d.date_created)
+    )
+    WHERE sensor_id is not null
+    AND location is not null
+    AND date_created is not null
 )
 ;
 
